ffcb69b5a84235c5864ae995f63e51f1947ead0f
Mikhail Korepanov <panyakor@gmail.com>
2019-01-13 19:01:43 +0300

    Refactor build

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-  input: 'index.js',
+  input: 'module.js',
   output: {
     file: 'dist/svelte-intl.js',
     format: 'umd',
