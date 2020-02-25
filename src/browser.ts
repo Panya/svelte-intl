62e1c8e8c66cc8f9e72cc4c516f47f70fdf9b785
Andrey Lechev <leechy@leechy.ru>
2020-02-25 22:12:07 +0100

    Use all of the `navigator.languages` to choose the right language (#4)
    * Update dependencies (mainly to get TypeScript to 3.7+ for optional chaining)
    
    * Update `getBrowserLocale` to use `navigator.languages` array with all user languages
    
    * Add `/dist` folder to the repo to install with npm
    
    * Remove dist folder to make a PR

--- src/browser.ts
+++ src/browser.ts
@@ -1,18 +1,26 @@
 import { locales, locale } from './core'
 import { get } from 'svelte/store'
 
+interface IENavigatorLanguage  {
+  userLanguage?: string
+}
+
 export const getBrowserLocale = (defaultLocale = 'en') => {
-  if (typeof window === 'undefined') return defaultLocale // ssr or node (sapper)
+  const targets = window?.navigator.languages || // user language preferences list
+    [
+      (window?.navigator as IENavigatorLanguage).userLanguage || // IE 10-
+      window?.navigator.language ||              // browser ui language
+      defaultLocale,                              // there is no window (sapper | node)
+    ]
+  const currentLocales = get(locales)
 
-  const target = window.navigator.language || window.navigator.languages[0]
-  if (target) {
-    const currentLocales = get(locales)
-    if (currentLocales.includes(target)) return target // exact match
+  for (let i = 0; i < targets.length; i = i + 1) {
+    if (currentLocales.includes(targets[i])) return targets[i] // exact match
 
-    const bestMatch = currentLocales.find(locale => target.startsWith(locale))
+    const bestMatch = currentLocales.find((locale: any) => targets[i].startsWith(locale))
     if (bestMatch) return bestMatch // en-US -> en
   }
 
   const currentLocale = get(locale)
-  return currentLocale || defaultLocale // default to current or param
+  return currentLocale || currentLocales[0] // default to current or just first
 }
