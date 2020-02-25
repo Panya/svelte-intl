28c5e91f47b57c18a14945ea680e23f99ef1b5c7
Andrey Lechev <leechy@leechy.ru>
2020-02-25 10:26:36 +0100

    Remove dist folder to make a PR

--- dist/browser.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const getBrowserLocale: (defaultLocale?: string) => any;
--- dist/util.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type DeepStringObj = {
-    [key: string]: string | DeepStringObj;
-};
-export declare type StringObj = {
-    [key: string]: string | undefined;
-};
-export declare const flatObject: (object: DeepStringObj) => StringObj;
